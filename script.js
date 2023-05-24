a4437f5193e21755e10942f8d8f9483b13449de0
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-05-23 19:47:16 -0600

    Add get methods and book input form

--- script.js
+++ script.js
@@ -13,44 +13,64 @@ function Book(title, author, pages, iHaveRead) {
             return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`
         }
     }
+    this.getTitle = function() {
+        return this.title;
+    }
+    this.getAuthor = function() {
+        return this.author;
+    }
+    this.getPages = function() {
+        return this.pages;
+    }
+    this.getReadStatus = function() {
+        return this.iHaveRead;
+    }
 }
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
-const cosmos = new Book('Cosmos', 'Carl Sagan', 432, false);
-
-console.log(theHobbit.info());
-console.log(cosmos.info());
 
 function addBookToLibrary(bookObject) {
     myLibrary.push(bookObject);
 }
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(cosmos);
-
 const tableBody = document.querySelector('tbody');
 
 function displayBookList(bookList) {
+    
+    while (tableBody.hasChildNodes()) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+
     bookList.forEach(element => {
         const row = document.createElement('tr');
         tableBody.appendChild(row);
 
         const tableData = document.createElement('td');
         row.appendChild(tableData);
-        tableData.textContent = element.title;
+        tableData.textContent = element.getTitle();
 
         const tableData2 = document.createElement('td');
         row.appendChild(tableData2);
-        tableData2.textContent = element.author;
+        tableData2.textContent = element.getAuthor();
 
         const tableData3 = document.createElement('td');
         row.appendChild(tableData3);
-        tableData3.textContent = element.pages;
+        tableData3.textContent = element.getPages();
 
         const tableData4 = document.createElement('td');
         row.appendChild(tableData4);
-        tableData4.textContent = element.iHaveRead;
+        tableData4.textContent = element.getReadStatus();
     });
 }
 
-displayBookList(myLibrary);
+const titleInput = document.querySelector('#title');
+const authorInput= document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readStatusInput = document.querySelector('#read-status');
+const submitButton = document.querySelector('.submit-button')
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatusInput.value);
+    addBookToLibrary(newBook);
+    displayBookList(myLibrary);
+    
+})
